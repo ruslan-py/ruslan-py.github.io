ab7502b4e312a302182cf8e43a488e6d692e862f
ruslan-py <79371022+ruslan-py@users.noreply.github.com>
2022-09-11 19:23:01 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+
+let btn = document.getElementById("btn");
+
+let item = "1";
+
+btn.addEventListener("click", function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вы выбрали товар 1!");
+        let item = '1'
+        tg.MainButton.show();
+    }
+});
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+    tg.sendData(item);
+});
