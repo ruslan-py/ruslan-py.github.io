98a4afe90d35e26516a474d9cf2d1b270af20e16
ruslan-py <79371022+ruslan-py@users.noreply.github.com>
2022-09-13 17:44:28 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ let btn = document.getElementById("btn");
 let item = "";
 
 let btn1 = document.getElementById("btn1");
-let btn1 = document.getElementById("btn2");
-let btn1 = document.getElementById("btn3");
-let btn1 = document.getElementById("btn4");
-let btn1 = document.getElementById("btn5");
-let btn1 = document.getElementById("btn6");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
 
 btn1.addEventListener("click", function(){
     if (tg.MainButton.isVisible) {
